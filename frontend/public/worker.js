55b147b7b2e1203d560e64c9a7b5d481aed63d3e
Maxime Dréau <maxime@dreau.fr>
2018-08-30 16:29:11 +0200

    fix and simplify the offline page

--- frontend/public/worker.js
+++ frontend/public/worker.js
@@ -1,57 +1,33 @@
-/* eslint-env serviceworker */
-/* global fetch location */
+/* eslint-env browser, serviceworker */
 
 const cacheName = 'cache-v1'
 
-const resourcesToCache = [
-  '/_offline',
-  '/main.css',
-  '/main.js',
-  '/manifest.json'
-]
+const offlinePageURL = '/_offline'
 
-self.addEventListener('install', function (event) {
+self.addEventListener('install', async function (event) {
   event.waitUntil(
-    caches.open(cacheName)
-      .then(cache => {
-        return cache.addAll(resourcesToCache)
-      })
-      .then(() => {
-        return self.skipWaiting()
-      })
-      .catch(err => {
-        console.log('could not setup cache', err)
-        throw err
-      })
+    (async () => {
+      const cache = await caches.open(cacheName)
+      await cache.add(offlinePageURL)
+      await self.skipWaiting()
+    })()
   )
 })
 
-// TODO expiration date
-// https://www.youtube.com/watch?v=ksXwaWHCW6k
-// https://github.com/deanhume/Service-Worker-Offline/blob/master/service-worker.js
 self.addEventListener('fetch', event => {
-  const req = event.request
-  const strippedURL = req.url.indexOf(location.origin) === 0 ? req.url.slice(location.origin.length) : req.cacheurl
-  let response
-  console.log(`fetch to ${strippedURL}`)
-  if (resourcesToCache.includes(strippedURL)) {
-    response = caches.open(cacheName)
-      .then(cache => cache.match(strippedURL))
-  } else {
-    if (isNavigationRequest(req)) {
-      response = networkOrOffline(req)
-    } else {
-      response = fetch(req)
-    }
-  }
-  event.respondWith(response)
+  const request = event.request
+  event.respondWith(isNavigationRequest(request) ? networkOrOffline(request) : fetch(request))
 })
 
 function isNavigationRequest (request) {
   return request.method === 'GET' && request.headers.get('accept').includes('text/html')
 }
 
-function networkOrOffline (req) {
-  return fetch(req)
-    .catch(() => caches.open(cacheName).then(cache => cache.match('/_offline')))
+async function networkOrOffline (request) {
+  try {
+    return await fetch(request)
+  } catch (err) {
+    const cache = await caches.open(cacheName)
+    return cache.match('/_offline')
+  }
 }
